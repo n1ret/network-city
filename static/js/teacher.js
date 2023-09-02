d28525b5adc101d5c9f174b0b7d502e55064874c
Alex <zovitemenakrutoj@gmail.com>
2023-09-03 01:20:51 +0500

    rollback ux changes

--- static/js/teacher.js
+++ static/js/teacher.js
@@ -55,6 +55,12 @@ document.body.onload=()=>{
         $(".loadercont").removeClass("hide");
     })
     $("#student-select").change((e)=>{
+        $("#viewinfo").html("");
+    });
+    $("#class-select").change();
+
+    $("#getinfo").submit(e=>{
+        e.preventDefault();
         const req={
             class:$("#class-select").val(),
             student:$("#student-select").val()
@@ -67,12 +73,6 @@ document.body.onload=()=>{
             })
         });
         $(".loadercont").removeClass("hide");
-    });
-    $("#class-select").change();
-    $("#student-select").change();
-
-    $("#getinfo").submit(e=>{
-        e.preventDefault();
     })
     
     $("#updinfo").submit(e=>{
