356940456af077fafc4da7103f3bf4df77084d21
Alex <zovitemenakrutoj@gmail.com>
2023-09-03 04:24:24 +0500

    fix adding empty page

--- export_passwords.py
+++ export_passwords.py
@@ -7,47 +7,51 @@ import dotenv
 import os
 
 dotenv.load_dotenv(".env")
-parser=argparse.ArgumentParser(prog='Export passwords',
-                    description='Export default passwords using hash of logins and sends word file using telegram bot')
+parser = argparse.ArgumentParser(
+    prog="Export passwords",
+    description="Export default passwords using hash of logins and sends word file using telegram bot",
+)
 
-parser.add_argument('-f', '--filename', default="passwords.docx")
-parser.add_argument('-u', '--userid', default=os.environ.get("EXPORT_USER_ID"))
-parser.add_argument('-b', '--bottoken', default=os.environ.get("EXPORT_BOT_TOKEN"))
-parser.add_argument('-s', '--saveonly', action='store_true')
+parser.add_argument("-f", "--filename", default="passwords.docx")
+parser.add_argument("-u", "--userid", default=os.environ.get("EXPORT_USER_ID"))
+parser.add_argument("-b", "--bottoken", default=os.environ.get("EXPORT_BOT_TOKEN"))
+parser.add_argument("-s", "--saveonly", action="store_true")
 
 args = parser.parse_args()
+
 
 def get_default_password(login):
     return md5(login.encode()).hexdigest()[:10]
 
+
 from backendtypes import DataBase
 
 with DataBase() as db:
-    classes=db.get_all_classes()
-    logins_bcls=[]
+    classes = db.get_all_classes()
+    logins_bcls = []
     for classr in classes:
-        users=db.get_users_by_class(classr)
+        users = db.get_users_by_class(classr)
         logins_bcls.append([user.login for user in users])
 
-doc=Document()
+doc = Document()
 
-j=len(classes)-1
+j = len(classes) - 1
 
-for ind,(classr,logins) in enumerate(zip(classes,logins_bcls)):
-    doc.add_heading(classr,1)
+for ind, (classr, logins) in enumerate(zip(classes, logins_bcls)):
+    doc.add_heading(classr, 1)
     doc.add_paragraph("diary130.ru")
     table = doc.add_table(rows=1, cols=2)
     row = table.rows[0].cells
-    for i, item in enumerate(["Логин","Пароль"]):
+    for i, item in enumerate(["Логин", "Пароль"]):
         p = row[i].paragraphs[0]
         p.add_run(item).bold = True
-    
+
     for login in logins:
         row = table.add_row().cells
         row[0].text = login
         row[1].text = get_default_password(login)
-    table.style="Table Grid"
-    if ind<j:
+    table.style = "Table Grid"
+    if ind < j:
         doc.add_page_break()
 
 sections = doc.sections
@@ -58,5 +62,9 @@ for section in sections:
     section.right_margin = Cm(2)
 doc.save(args.filename)
 
-if not(args.saveonly):
-    rq.post(f"https://api.telegram.org/bot{args.bottoken}/sendDocument", data={'chat_id': args.userid}, files={'document': open(args.filename,"rb")})
+if not (args.saveonly):
+    rq.post(
+        f"https://api.telegram.org/bot{args.bottoken}/sendDocument",
+        data={"chat_id": args.userid},
+        files={"document": open(args.filename, "rb")},
+    )
