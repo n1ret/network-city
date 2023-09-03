351a8d974cbfa28719f53035337a1d6c48ebe7d5
Alex <zovitemenakrutoj@gmail.com>
2023-09-03 16:38:37 +0500

    fix

--- insert_user.py
+++ insert_user.py
@@ -3,18 +3,20 @@ import argparse
 
 parser = argparse.ArgumentParser(prog="python3 insert_user.py")
 
-parser.add_argument("fullname")
-parser.add_argument("school_class", nargs=1, default="")
+parser.add_argument("fullname", nargs=2)
+parser.add_argument("school_class", nargs='?', default="")
 parser.add_argument("-t", "--teacher", action="store_true")
 
 args = parser.parse_args()
 
+fullname = ' '.join(args.fullname)
+
 with DataBase() as db:
     used_logins = db.get_used_logins()
-    login = next_available_login(used_logins, args.fullname)
+    login = next_available_login(used_logins, fullname)
     password_hash = get_default_password_hash(login)
     uid = db.insert_or_update_user(
-        args.fullname, args.school_class, login, password_hash, parser.teacher
+        fullname, args.school_class, login, password_hash, parser.teacher
     )
 
-print(f"Created user #{uid}\n Fullname: {args.fullname}\n Login: {login}\n Password: {get_default_password(login)}")
+print(f"Created user #{uid}\n Fullname: {fullname}\n Login: {login}\n Password: {get_default_password(login)}")
